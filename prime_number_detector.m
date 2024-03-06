061fd5da9486aef378487353af386bcd32092131
Sina Ghasemi Samie <162462153+sinaghasemisamie@users.noreply.github.com>
2024-03-06 18:22:15 +0330

    Add files via upload

---
+++ prime_number_detector.m
@@ -0,0 +1,19 @@
+%%% Rebron 
+%%% Prime Number Detecror
+%%% Difficulty: Amateur
+%% Basic Structures
+clear 
+close all
+clc
+%% Let's Create Prime Number Detector
+n = input('Enter Your Desired Number'); % User's Desired Number
+n1 = 1:n; % Create Vector from 1 to n
+R = rem(n,n1); % Reminder of n./n1 Division
+loc = find(R == 0); % Location of R = 0
+if length(loc)>2
+     disp('Not Prime')
+elseif   length(loc)<2
+     disp('null')
+elseif length(loc) == 2
+     disp('Prime')
+end
